09ad53f08caa3b40d3e7b617407b3ec883523726
JivaniParth <parthjivani1504@gmail.com>
2025-11-01 23:10:32 +0530

    added frontend files and integrated it with backend

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,25 @@ const port = process.env.PORT || 4000;
 
 app.use(express.json());
 
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 // Routes
 const recommendations = require("./routes/recommendations");
 app.use("/api/recommendations", recommendations);
+
+const products = require("./routes/products");
+app.use("/api/products", products);
+
 // A/B testing routes
 const abRoutes = require("./routes/ab");
 app.use("/api/ab", abRoutes);
+
 // Events (impression/click) routes
 const eventsRoutes = require("./routes/events");
 app.use("/api/events", eventsRoutes);
